ad2276c13ef43d84394fd362f1a7bbc527be8e93
Sinisa <sinisa_cavic@hotmail.de>
2015-12-16 09:17:10 +0100

    sieht schon besser aus

--- src/TestEmailClientFrame.java
+++ src/TestEmailClientFrame.java
@@ -1,6 +1,8 @@
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.Dimension;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,11 +11,11 @@ import javax.swing.JPanel;
 public class TestEmailClientFrame {
 
 	private JFrame frame;
-	private JPanel topBorderElements = new JPanel(new FlowLayout());
+	private JPanel topBorderElements = new JPanel();
 	private JButton newMailButton = new JButton("New E-Mail");
 	private JButton logOutButton = new JButton("Log out");
 	private JButton answerMailButton = new JButton("Re");
-	private JLabel ownEmailAddress = new JLabel("Grab address and put it here");
+	private JLabel ownEmailAddress = new JLabel("Grab E-mail address and put it here");
 
 	public TestEmailClientFrame() {
 		this("No title", 800, 600);
@@ -31,9 +33,13 @@ public class TestEmailClientFrame {
 
 	private void showDefault() {
 		frame.setLayout(new BorderLayout());
+		topBorderElements.setLayout(new BoxLayout(topBorderElements,BoxLayout.X_AXIS));
 		topBorderElements.add(newMailButton);
+		topBorderElements.add(Box.createRigidArea(new Dimension(10,0)));
 		topBorderElements.add(answerMailButton);
+		topBorderElements.add(Box.createHorizontalGlue());
 		topBorderElements.add(logOutButton);
+		topBorderElements.add(Box.createRigidArea(new Dimension(10,0)));
 		topBorderElements.add(ownEmailAddress);
 		frame.getContentPane().add(topBorderElements, BorderLayout.PAGE_START);
 		
